2207d1796627731a7c75475db84bc265c45bd8d0
4masaka <in9lude@gmail.com>
2019-11-03 17:58:16 +0900

    docstring

--- pyne/api.py
+++ pyne/api.py
@@ -17,40 +17,74 @@ from .protocol import LineProtocolFactory
 
 
 class Api(metaclass=ABCMeta):
-    pass
+    """Apiのメタクラス
+
+    """
 
 
 class ApiFactory(metaclass=ABCMeta):
+    """Apiファクトリのメタクラス
+
+    Attributes:
+        host: APIホスト
+    """
+
     def __init__(self, host: str):
         self.host = host
 
     @abstractmethod
     def create(self, path: str, headers: Dict):
+        """Apiクラスを生成する関数
+
+        Args:
+            path: APIエンドポイントのpath
+            headers: httpヘッダー
+        """
         NotImplementedError()
 
     def get_provider(self, path, headers) -> FServiceProvider:
+        """FServiceProviderクラスを生成する関数
+
+        http clientとprotocol factoryを取得し、FServiceProviderに渡す。
+
+        Args:
+            path: APIエンドポイントのpath
+            headers: httpヘッダー
+
+        Return:
+            `FServiceProvider`
+        """
         http_client = HttpClientFactory(self.host).get_client(path, headers)
         protocol_factory = TCompactProtocolAcceleratedFactory()
-        provider = FServiceProvider(http_client, LineProtocolFactory(protocol_factory))
-        return provider
+
+        return FServiceProvider(http_client, LineProtocolFactory(protocol_factory))
 
 
 class TalkApi(Api, FTalkServiceClient):
-    pass
+    """TalkServiceのApiクラス
+
+    """
 
 
 class TalkApiFactory(ApiFactory):
+    """`TalkApi`のファクトリクラス
 
+    """
     def create(self, path: str, headers: Dict) -> TalkApi:
         provider = self.get_provider(path, headers)
         return TalkApi(provider)
 
 
 class AuthApi(Api, FAuthServiceClient):
-    pass
+    """AuthServiceのApiクラス
+
+    """
 
 
 class AuthApiFactory(ApiFactory):
+    """`AuthApi`のファクトリクラス
+
+    """
     def create(self, path: str, headers: Dict) -> AuthApi:
         provider = self.get_provider(path, headers)
         return AuthApi(provider)
