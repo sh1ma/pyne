f038805a2b3fe620a00330e9112c96b67868c843
4masaka <in9lude@gmail.com>
2019-11-06 07:31:59 +0900

    lineバージョンを2.2.0から2.3.0にした

--- pyne/config.py
+++ pyne/config.py
@@ -9,7 +9,7 @@ import dataclasses
 @dataclasses.dataclass
 class Config:
     host: str = "legy-jp-addr.line.naver.jp"
-    line_app: str = "CHROMEOS\t2.2.2\tChrome_OS\t1"
+    line_app: str = "CHROMEOS\t2.3.0\tChrome_OS\t1"
     user_agent: str = "Mozilla/5.0 Chrome/77.0.3865.120"
     system_name: str = "pyne"
 
