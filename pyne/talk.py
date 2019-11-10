d76ec07053f6fdbfccfb53399d6ea8d0278db775
4masaka <in9lude@gmail.com>
2019-11-11 00:00:25 +0900

    talkservice

--- pyne/talk.py
+++ pyne/talk.py
@@ -9,12 +9,26 @@ from typing import Dict
 
 from .api import Api, ApiFactory
 from .line_thrift.line import FTalkServiceClient
+from .line_thrift.line import Profile
+from .line_thrift.line import AuthQrcode
+
+from frugal.context import FContext
 
 
 class TalkApi(Api, FTalkServiceClient):
     """TalkServiceのApiクラス
 
     """
+
+    async def get_profile(self) -> Profile:
+        profile: Profile = await self.getProfile(FContext())
+        return profile
+
+    async def issue_qr_verifier(self, keep_logged_in: bool, system_name: str) -> str:
+        qr_code: AuthQrcode = await self.getAuthQrcode(
+            FContext(), keep_logged_in, system_name, False
+        )
+        return qr_code.verifier
 
 
 class TalkApiFactory(ApiFactory):
