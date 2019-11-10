15403ef5507741656aa3b13ae43e5d7a1b8a0183
4masaka <in9lude@gmail.com>
2019-11-10 23:59:38 +0900

    loginの実装

--- pyne/auth.py
+++ pyne/auth.py
@@ -9,13 +9,19 @@ from typing import Dict
 
 from .api import Api, ApiFactory
 from .line_thrift.line import FAuthServiceClient
+from .line_thrift.line import LoginResult, LoginRequest
 
+from frugal.context import FContext
 
 class AuthApi(Api, FAuthServiceClient):
     """AuthServiceのApiクラス
 
     """
 
+    async def login(self, login_request: LoginRequest) -> LoginResult:
+        login_result: LoginResult = await self.loginZ(FContext(), login_request)
+
+        return login_result
 
 class AuthApiFactory(ApiFactory):
     """`AuthApi`のファクトリクラス
